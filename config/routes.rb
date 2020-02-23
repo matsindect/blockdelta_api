ca04df79150f3a1b48275e8053efd7c3fa626a52
matsindect <matsindect@gmail.com>
2020-02-23 02:40:46 -0800

    api versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
 
-  post   '/users/signup'   => 'users#create'
-  post   '/users/login'    => 'users#login'
-  get    '/users'          => 'users#index'
-  get    '/users/current'  => 'users#current'
-  patch  '/user/:id'       => 'users#update'
-  delete '/user/:id'       => 'users#destroy'
+      #Users resource
+        post   '/users/signup'   => 'users#create'
+        post   '/users/login'    => 'users#login'
+        get    '/users'          => 'users#index'
+        get    '/users/current'  => 'users#current'
+        patch  '/user/:id'       => 'users#update'
+        delete '/user/:id'       => 'users#destroy'
+
+      #blogs resource
+      
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
